a39b4529167508eb3e71858ccf8a345641e55ac6
BoonCaTre <phannhatminh809@gmail.com>
2025-11-14 17:33:33 +0700

    Refactor SinhVien class and update input/output methods

--- CPP0613 - DanhSachDoiTuongSinhVien3.cpp
+++ CPP0613 - DanhSachDoiTuongSinhVien3.cpp
@@ -1,77 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int counter = 1;
+int stt = 1;
 
 class SinhVien {
-private:
-    string maSV, hoTen, lop, ngaySinh;
-    float gpa;
-    
 public:
-    SinhVien() {
-        maSV = hoTen = lop = ngaySinh = "";
-        gpa = 0;
-    }
-    
-    friend istream& operator >> (istream& in, SinhVien& sv) {
-        in.ignore();
-        getline(in, sv.hoTen);
-        getline(in, sv.lop);
-        getline(in, sv.ngaySinh);
-        in >> sv.gpa;
-        
-        sv.maSV = "B20DCCN" + string(3 - to_string(counter).length(), '0') + to_string(counter);
-        counter++;
-        
-        sv.chuanHoaHoTen();
-        sv.chuanHoaNgaySinh();
-        
-        return in;
-    }
-    
-    friend ostream& operator << (ostream& out, const SinhVien& sv) {
-        out << sv.maSV << " " << sv.hoTen << " " << sv.lop << " " 
-            << sv.ngaySinh << " " << fixed << setprecision(2) << sv.gpa;
-        return out;
-    }
-    
-    void chuanHoaHoTen() {
-        stringstream ss(hoTen);
-        string word, result;
-        while (ss >> word) {
-            word[0] = toupper(word[0]);
-            for (int i = 1; i < word.length(); i++) {
-                word[i] = tolower(word[i]);
-            }
-            result += word + " ";
+    string msv, ten, lop, nsinh;
+    float gpa;
+
+    friend istream &operator >> (istream &is, SinhVien &a) {
+        scanf("\n");
+        getline(is, a.ten);
+        is >> a.lop >> a.nsinh >> a.gpa;
+
+        string str = "", temp;
+        stringstream ss(a.ten);
+        while (ss >> temp) {
+            transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+            temp[0] = toupper(temp[0]);
+            str += temp + " ";
         }
-        if (!result.empty()) {
-            result.pop_back();
-        }
-        hoTen = result;
+        str.pop_back();
+        a.ten = str;
+
+        string s = to_string(stt++);
+        while (s.length() < 3)
+            s = '0' + s;
+        a.msv = "B20DCCN" + s;
+
+        if (a.nsinh[2] != '/') a.nsinh.insert(0, "0");
+        if (a.nsinh[5] != '/') a.nsinh.insert(3, "0");
+        return is;
     }
-    
-    void chuanHoaNgaySinh() {
-        stringstream ss(ngaySinh);
-        string day, month, year;
-        getline(ss, day, '/');
-        getline(ss, month, '/');
-        getline(ss, year);
-        
-        if (day.length() == 1) day = "0" + day;
-        if (month.length() == 1) month = "0" + month;
-        
-        ngaySinh = day + "/" + month + "/" + year;
-    }
-    
-    float getGPA() const {
-        return gpa;
+
+    friend ostream &operator << (ostream &os, SinhVien a) {
+        os << a.msv << " " << a.ten << " " << a.lop << " " << a.nsinh << " ";
+        os << fixed << setprecision(2) << a.gpa << "\n";
+        return os;
     }
 };
 
-bool cmp(const SinhVien& a, const SinhVien& b) {
-    return a.getGPA() > b.getGPA();
+bool cmp(SinhVien a, SinhVien b) {
+    return a.gpa > b.gpa;
 }
 
 void sapxep(SinhVien ds[], int N) {
@@ -87,7 +57,7 @@ int main(){
     }
     sapxep(ds, N);
     for(i=0;i<N;i++){
-        cout << ds[i] << endl;
+        cout << ds[i];
     }
     return 0;
 }
